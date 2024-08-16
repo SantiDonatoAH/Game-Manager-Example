d74853b1db356194c1af6ee79fd30058f1952575
SantiDonatoAH <santivdonato@gmail.com>
2024-08-16 10:29:50 -0300

    actResuelta

---
+++ Assets/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance; 
+
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangeScene("Game");
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            ChangeScene("Menu");
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
